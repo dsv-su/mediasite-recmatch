95d187ef1f4a9c6b4f0c324ea2f3a46e0e150abd
Per Olofsson <pelle@dsv.su.se>
2015-11-16 17:33:56 +0100

    remove scheduling stuff from api

--- src/MediasiteClient.php
+++ src/MediasiteClient.php
@@ -41,33 +41,6 @@ class MediasiteClient {
         return $this->get($objects.'?$top=100000');
     }
 
-    function getRecorder($id) {
-        return $this->get("Recorders[$id]");
-    }
-
-    function getRecurrences($id) {
-        return $this->getAll("Schedules($id)/Recurrences");
-    }
-
-    function getSchedules() {
-        if (!isset($this->schedules)) {
-            $raw_schedules = $this->getAll('Schedules');
-            $this->schedules = [];
-            foreach ($raw_schedules as $schedule) {
-                $this->schedules[$schedule['Id']] = $schedule;
-            }
-        }
-        return $this->schedules;
-    }
-
-    function getSchedule($id) {
-        return $this->getSchedules()[$id];
-    }
-
-    function scheduleExists($id) {
-        return isset($this->getSchedules()[$id]);
-    }
-
     function findFolder($name, $parent_id) {
         if (!isset($this->folders)) {
             $this->folders = $this->getAll('Folders');
@@ -88,23 +61,5 @@ class MediasiteClient {
                                       'ParentFolderId' => $parent_id,
                                       'IsShared' => true
                                   ]])->json()['Id'];
-    }
-
-    function updateSchedule($id, $params) {
-        return $this->request('PATCH', "Schedules('$id')", [ 'json' => $params ]);
-    }
-
-    function getScheduleTags($schedule_id) {
-        return get("Schedules('$schedule_id')/Tags");
-    }
-
-    function addScheduleTag($schedule_id, $tag) {
-        $this->request('POST', "Schedules('$scheduleid')/Tags", ['json' => [
-                               'Tag' => $tag
-                           ]]);
-    }
-
-    function removeScheduleTag($schedule_id, $tag_id) {
-        $this->request('DELETE', "Schedules('$schedule_id')/Tags('$tag_id')");
     }
 }
