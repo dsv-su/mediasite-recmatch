ac4e3e4a12458173f2fbcea6540a66a8e9b49228
Per Olofsson <pelle@dsv.su.se>
2015-11-17 11:24:10 +0100

    stuff

--- src/Matcher.php
+++ src/Matcher.php
@@ -17,6 +17,30 @@ class Matcher
         return null;
     }
 
+    private function process($presentation) {
+        //print_r($presentation);
+        $room = $this->findRoom($presentation);
+
+        if (null === $room) {
+            echo "Could not match room for " . $presentation['Title'] . "\n";
+            return;
+        }
+
+        $start_time = \DateTime::createFromFormat(
+            'Y-m-d\TH:i:s',
+            $presentation['RecordDateLocal']
+        );
+
+        $seconds = intval(round(intval($presentation['Duration']) / 1000));
+        $end_time = clone $start_time;
+        $end_time->add(new \DateInterval("PT" . $seconds . "S"));
+
+        echo "# Presentation\n";
+        echo "Room: " . $room['name'] . "\n";
+        echo "Start time: " . $start_time->format('r') . "\n";
+        echo "End time: " . $end_time->format('r') . "\n";
+    }
+
     public function run()
     {
         $dropoff_presentations = Mediasite::getPresentationsInFolder(
@@ -24,14 +48,7 @@ class Matcher
         );
 
         foreach ($dropoff_presentations as $presentation) {
-            //print_r($presentation);
-            $room = $this->findRoom($presentation);
-
-            if (null === $room) {
-                echo "Could not match room for " . $presentation['Title'] . "\n";
-            }
-
-            
+            $this->process($presentation);
         }
     }
 }
